a061df07c1fdceb17088d4895111fd84e9507de6
Cattttttttt <neko@qwq.icu>
2021-07-12 21:41:23 +0800

    🐳 chore(): add base64

---
+++ src/app.tsx
@@ -0,0 +1,27 @@
+import Layout from './mvc_layout';
+import { Box, makeStyles } from '@material-ui/core';
+import clsx from 'clsx';
+
+const useStyles = makeStyles({
+  root: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#D9D9D9',
+    height: '100vh',
+  },
+});
+
+const App = () => {
+  const styles = useStyles();
+
+  return (
+    <Box
+      className={clsx(styles.root)}
+    >
+      <Layout />
+    </Box>
+  );
+};
+
+export default App;
