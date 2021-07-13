0a7dc3308d115fe3b34a54b3c9559e00fb38f5da
Cattttttttt <neko@qwq.icu>
2021-07-13 15:02:32 +0800

    🦄 refactor(reducers): extract constants

--- src/mvc_constants.tsx
+++ src/mvc_constants.tsx
@@ -1,1 +1,32 @@
+import { RadioButtonUncheckedOutlined, CheckCircleOutline } from '@material-ui/icons';
+import { makeStyles } from "@material-ui/core";
+import { TodoStatusType } from './mvc_types';
+
 export const STORAGE_LIST = 'STORAGE_LIST'
+
+export const TODO_CHANGE_STATUS = 'TODO_CHANGE_STATUS';
+
+export const TODO_STATUS_ACTIVE = 'TODO_STATUS_ACTIVE';
+export const TODO_STATUS_COMPLETED = 'TODO_STATUS_COMPLETED';
+export const TODO_STATUS_DEFAULT = TODO_STATUS_ACTIVE;
+
+export const TodoStatusLists: TodoStatusType[] = [TODO_STATUS_ACTIVE, TODO_STATUS_COMPLETED];
+
+export const TodoStatusComponentMap = {
+  TODO_STATUS_ACTIVE: <RadioButtonUncheckedOutlined />,
+  TODO_STATUS_COMPLETED: <CheckCircleOutline />,
+};
+
+export const TodoStatusTextMap = makeStyles({
+  root: {
+    flexGrow: 1,
+    wordBreak: 'break-all',
+    transition: 'all 0.4s',
+  },
+  [TODO_STATUS_ACTIVE]: {},
+  [TODO_STATUS_COMPLETED]: {
+    color: '#D9D9D9',
+    textDecoration: 'line-through solid #D9D9D9 2px',
+    transition: 'all 0.4s',
+  },
+});
--- src/mvc_reducers.tsx
+++ src/mvc_reducers.tsx
@@ -1,36 +1,6 @@
-import { ItemProperties } from "./mvc_layout";
-import { RadioButtonUncheckedOutlined, CheckCircleOutline } from '@material-ui/icons';
-import { Box, makeStyles, styled } from "@material-ui/core";
-
-export const TODO_CHANGE_STATUS = 'TODO_CHANGE_STATUS';
-export type TodoAction = typeof TODO_CHANGE_STATUS;
-
-export const TODO_STATUS_ACTIVE = 'TODO_STATUS_ACTIVE';
-export const TODO_STATUS_COMPLETED = 'TODO_STATUS_COMPLETED';
-export const TODO_STATUS_DEFAULT = TODO_STATUS_ACTIVE;
-
-export const TodoStatusLists: TodoStatusType[] = [TODO_STATUS_ACTIVE, TODO_STATUS_COMPLETED];
-
-export const TodoStatusComponentMap = {
-  TODO_STATUS_ACTIVE: <RadioButtonUncheckedOutlined />,
-  TODO_STATUS_COMPLETED: <CheckCircleOutline />,
-};
-
-export const TodoStatusTextMap = makeStyles({
-  root: {
-    flexGrow: 1,
-    wordBreak: 'break-all',
-    transition: 'all 0.4s',
-  },
-  [TODO_STATUS_ACTIVE]: {},
-  [TODO_STATUS_COMPLETED]: {
-    color: '#D9D9D9',
-    textDecoration: 'line-through solid #D9D9D9 2px',
-    transition: 'all 0.4s',
-  },
-});
-
-export type TodoStatusType = keyof typeof TodoStatusComponentMap;
+import { ItemProperties } from "./mvc_types";
+import { TodoAction } from './mvc_types';
+import { TodoStatusLists } from './mvc_constants';
 
 export const TodoReducers = (state: ItemProperties, action: TodoAction) => {
   switch (action) {
